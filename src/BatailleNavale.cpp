59ced0d27a220a8d2ff6b9cc242528951ac7cde1
nadineducegep <st-amand.nadine@cgmatane.qc.ca>
2017-10-03 15:08:27 -0400

    Main

---
+++ src/BatailleNavale.cpp
@@ -0,0 +1,15 @@
+//============================================================================
+// Name        : BatailleNavale.cpp
+// Author      : Nadine Giasson St-Amand
+// Version     :
+// Copyright   : (c) LudiJeu
+// Description : Une bataille navale pour tester des classes bateau
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "!!! Je vous répondrai par la bouche de mes canons !!!" << endl; // prints !!! Je vous répondrai par la bouche de mes canons !!!
+	return 0;
+}
